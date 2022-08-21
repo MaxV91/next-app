c75e8369698b4feb3b40fff205a8e665ee87c25d
Maxim K <max.kotsurenko@gmail.com>
2022-08-21 13:36:52 +0300

    HOC withLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import {Button, Htag, P, Rating, Tag} from '../components';
 import {useState} from 'react';
+import { withLayout } from '../layout/Layout';
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
@@ -19,3 +20,5 @@ export default function Home(): JSX.Element {
     </>
   );
 }
+
+export default withLayout(Home);
