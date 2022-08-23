50ff6be636294e744da74e1e195cf8c10c533437
Maxim K <max.kotsurenko@gmail.com>
2022-08-23 17:59:16 +0300

    added api, ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import Head from 'next/head';
+import {AppProps} from 'next/app';
 
-// @ts-ignore
 function MyApp({Component, pageProps}: AppProps): JSX.Element {
   return <>
     <Head>
