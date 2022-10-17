7bdf2198877e4dd2670b457232dfbd3812e15fa5
Maxim K <max.kotsurenko@gmail.com>
2022-10-17 22:31:58 +0300

    advantages

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,5 +1,5 @@
 import {TopPageComponentProps} from './TopPageComponent.props';
-import {HhData, Htag, Tag} from '../../components';
+import {Advantages, HhData, Htag, P, Tag} from '../../components';
 import styles from './TopPageComponent.module.css'
 import {TopLevelCategory} from '../../interfaces/page.interface';
 
@@ -18,7 +18,14 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
         <Htag tag='h2'>Вакансии - {page.category}</Htag>
         <Tag color='red' size='m'>hh.ru</Tag>
       </div>
-      {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />}
+      {firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
+      {page.advantages && page.advantages.length > 0 && <>
+        <Htag tag='h2'>Преимущества</Htag>
+        <Advantages advantages={page.advantages} />
+      </>}
+      {page.seoText && <P>{page.seoText}</P>}
+      <Htag tag='h2'>Получаемые навыки</Htag>
+      {page.tags.map(t=> <Tag key={t} color='primary'>{t}</Tag>)}
     </div>
   );
 }
