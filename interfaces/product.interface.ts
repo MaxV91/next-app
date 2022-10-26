3237072f2e62f60939fefed1f0e52894274e51b8
Maxim K <max.kotsurenko@gmail.com>
2022-10-26 23:29:46 +0300

    product styles

--- interfaces/product.interface.ts
+++ interfaces/product.interface.ts
@@ -33,6 +33,6 @@ export interface ProductModel {
   reviews: ReviewModel[];
   reviewCount: number;
   reviewAvg?: number;
-  advantages: string;
-  disadvantages: string;
+  advantages?: string;
+  disadvantages?: string;
 }
