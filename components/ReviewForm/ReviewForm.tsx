c5c46505e44857d66cf51d73a8068da849c377c1
Maxim K <max.kotsurenko@gmail.com>
2022-10-31 00:10:44 +0300

    useForm

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -6,20 +6,33 @@ import {Input} from '../Input/Input';
 import {Rating} from '../Rating/Rating';
 import {Textarea} from '../Textarea/Textarea';
 import {Button} from '../Button/Button';
+import {useForm, Controller} from 'react-hook-form';
+import {IReviewForm} from './ReviewForm.interface';
 
 export const ReviewForm = ({productId, className, ...props}: ReviewFormProps): JSX.Element => {
+  const { register, control, handleSubmit } = useForm<IReviewForm>();
+
+  const onSubmit = (data: IReviewForm) => {
+    console.log(data);
+  }
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className={cn(styles.reviewForm, className)}
            {...props}
       >
-        <Input placeholder='Имя'/>
-        <Input className={styles.title} placeholder='Заголовок отзыва'/>
+        <Input {...register('name')} placeholder='Имя'/>
+        <Input {...register('title')} className={styles.title} placeholder='Заголовок отзыва'/>
         <div className={styles.rating}>
           <span>Оценка:</span>
-          <Rating rating={0}/>
+          <Controller
+            control={control}
+            render={({ field }) => (
+              <Rating isEditable rating={field.value} setRating={field.onChange} />
+            )}
+            name='rating' />
         </div>
-        <Textarea className={styles.description} placeholder='Тест отзыва'/>
+        <Textarea {...register('description')} className={styles.description} placeholder='Тест отзыва'/>
         <div className={styles.submit}>
           <Button appearance='primary'>Отправить</Button>
           <span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
@@ -32,6 +45,6 @@ export const ReviewForm = ({productId, className, ...props}: ReviewFormProps): J
         </div>
         <CloseIcon className={styles.close}/>
       </div>
-    </>
+    </form>
   );
 }
