1314fed536470c599a819d1baf50ab45f7f9be5d
Maxim K <max.kotsurenko@gmail.com>
2022-11-01 00:03:58 +0300

    send post request, refs

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -4,7 +4,7 @@ import cn from 'classnames';
 import {useEffect, useState, KeyboardEvent, forwardRef, ForwardedRef} from 'react';
 import StarIcon from './star.svg';
 
-export const Rating = forwardRef(({isEditable = false, rating, setRating, ...props}: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
+export const Rating = forwardRef(({isEditable = false, error, rating, setRating, ...props}: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
   const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
 
   useEffect(() => {
@@ -55,8 +55,11 @@ export const Rating = forwardRef(({isEditable = false, rating, setRating, ...pro
   }
 
   return (
-    <div {...props} ref={ref}>
+    <div {...props} ref={ref} className={cn(styles.ratingWrapper, {
+      [styles.error]: error
+    })}>
       {ratingArray.map((r, i) => (<span key={i}>{r}</span>))}
+      {error && <span className={styles.errorMessage}>{error.message}</span>}
     </div>
   );
 })
