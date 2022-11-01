2c49dd70818d950bd75d1a6a0c7e7c9264e4565f
Maxim K <max.kotsurenko@gmail.com>
2022-11-01 16:53:43 +0300

    hook scrollY

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -5,6 +5,7 @@ import {TopLevelCategory} from '../../interfaces/page.interface';
 import {SortEnum} from '../../components/Sort/Sort.props';
 import {useEffect, useReducer} from 'react';
 import {sortReducer} from './sort.reducer';
+import {useScrollY} from '../../hooks/useScrollY';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
   const [{products: sortedProducts, sort}, dispatchSort] = useReducer(sortReducer, { products, sort: SortEnum.Rating });
