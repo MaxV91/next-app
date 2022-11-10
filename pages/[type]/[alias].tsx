8e043076c6657eb133f25e9665bc21ec2b47509c
Maxim K <max.kotsurenko@gmail.com>
2022-11-10 23:56:35 +0300

    docker container

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -9,26 +9,27 @@ import {firstLevelMenu} from '../../helpers/helpers';
 import {TopPageComponent} from '../../page-components';
 import {API} from '../../helpers/api';
 import Head from 'next/head';
+import {Error404} from '../404';
 
 function TopPage({firstCategory, page, products}: TopPageProps): JSX.Element {
+  if (!page || !products) {
+    return <Error404/>;
+  }
 
   return (
     <>
-      {page && products &&
-      <>
-        <Head>
-          <title>{page.metaTitle}</title>
-          <meta name='description' content={page.metaDescription}/>
-          <meta property='og:title' content={page.metaTitle}/>
-          <meta property='og:description' content={page.metaDescription}/>
-          <meta property='og:type' content='article'/>
-        </Head>
-        <TopPageComponent
-          firstCategory={firstCategory}
-          page={page}
-          products={products}
-        />
-      </>}
+      <Head>
+        <title>{page.metaTitle}</title>
+        <meta name='description' content={page.metaDescription}/>
+        <meta property='og:title' content={page.metaTitle}/>
+        <meta property='og:description' content={page.metaDescription}/>
+        <meta property='og:type' content='article'/>
+      </Head>
+      <TopPageComponent
+        firstCategory={firstCategory}
+        page={page}
+        products={products}
+      />
     </>);
 }
 
@@ -50,51 +51,51 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps<TopPageProps> = async ({params}: GetStaticPropsContext<ParsedUrlQuery>) => {
   if (!params) {
-  return {
-  notFound: true
-}
-}
+    return {
+      notFound: true
+    }
+  }
   const firstCategoryItem = firstLevelMenu.find(m => m.route == params.type);
   if (!firstCategoryItem) {
-  return {
-  notFound: true
-}
-}
+    return {
+      notFound: true
+    }
+  }
 
   try {
-  const {data: menu} = await axios.post<MenuItem[]>(API.topPage.find, {
-  firstCategory: firstCategoryItem.id
-});
-  if (menu.length == 0) {
-  return {
-  notFound: true
-}
-}
-  const {data: page} = await axios.get<TopPageModel>(API.topPage.byAlias + params.alias);
-  const {data: products} = await axios.post<ProductModel[]>(API.product.find, {
-  category: page.category,
-  limit: 10
-});
-  return {
-  props: {
-  menu,
-  firstCategory: firstCategoryItem.id,
-  page,
-  products
-}
-}
-} catch {
-  return {
-  notFound: true
-};
-}
+    const {data: menu} = await axios.post<MenuItem[]>(API.topPage.find, {
+      firstCategory: firstCategoryItem.id
+    });
+    if (menu.length == 0) {
+      return {
+        notFound: true
+      }
+    }
+    const {data: page} = await axios.get<TopPageModel>(API.topPage.byAlias + params.alias);
+    const {data: products} = await axios.post<ProductModel[]>(API.product.find, {
+      category: page.category,
+      limit: 10
+    });
+    return {
+      props: {
+        menu,
+        firstCategory: firstCategoryItem.id,
+        page,
+        products
+      }
+    }
+  } catch {
+    return {
+      notFound: true
+    };
+  }
 
 
 }
 
-  interface TopPageProps extends Record
+interface TopPageProps extends Record
   <string
-  , unknown> {
+    , unknown> {
   menu: MenuItem[];
   firstCategory: TopLevelCategory;
   page: TopPageModel;
